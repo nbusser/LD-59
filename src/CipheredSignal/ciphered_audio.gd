3ac119d5528e0d8ea40eb4ce88d60b002e7df74e
Antoine Brûlé <antoine.brule@netc.eu>
2026-04-18 23:29:43 +0200

    add speed input

--- src/CipheredSignal/ciphered_audio.gd
+++ src/CipheredSignal/ciphered_audio.gd
@@ -1,6 +1,8 @@
 class_name CipheredAudio
 extends CipheredSignal
 
+@export var speed_input: SignalInput
+
 var source: AudioStream:
 	set(new_source):
 		source = new_source
@@ -32,6 +34,8 @@ func _reset() -> void:
 
 
 func _ready() -> void:
+	speed_input.signal_input_changed.connect(_speed_input_changed)
+
 	_reset()
 
 	player = AudioStreamPlayer.new()
@@ -39,6 +43,10 @@ func _ready() -> void:
 	player.stream = source
 	player.bus = "CipheredSignal"
 	player.call_deferred("play")
+
+
+func _speed_input_changed(value: float) -> void:
+	player.pitch_scale = value * 2
 
 
 func _render() -> void:
