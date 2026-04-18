9e9f400c07e35a06f4a7459a38c298537ab65535
Antoine Brûlé <antoine.brule@netc.eu>
2026-04-18 22:51:47 +0200

    add audio spectrum visualizer

--- src/CipheredSignal/ciphered_audio.gd
+++ src/CipheredSignal/ciphered_audio.gd
@@ -1,12 +1,44 @@
 class_name CipheredAudio
 extends CipheredSignal
 
-var source: AudioStream
+var source: AudioStream:
+	set(new_source):
+		source = new_source
+		_reset()
+		_render()
+
+var player: AudioStreamPlayer
+
 var _transformed_audio: AudioStream
 
 
+func _reset() -> void:
+	if source == null:
+		return
+
+	var prng = RandomNumberGenerator.new()
+	prng.seed = source.resource_scene_unique_id.hash()
+
+	if source is AudioStreamMP3:
+		source.loop = true
+	elif source is AudioStreamOggVorbis:
+		source.loop = true
+	elif source is AudioStreamWAV:
+		source.loop_mode = AudioStreamWAV.LOOP_FORWARD
+	else:
+		push_error("Unsupported audio stream type: %s" % source)
+
+	player.stream = source
+
+
 func _ready() -> void:
-	pass
+	_reset()
+
+	player = AudioStreamPlayer.new()
+	add_child(player)
+	player.stream = source
+	player.bus = "CipheredSignal"
+	player.call_deferred("play")
 
 
 func _render() -> void:
--- src/Monitor/monitor.gd
+++ src/Monitor/monitor.gd
@@ -1,8 +1,27 @@
 class_name Monitor extends Control
 
+# audio spectrum visualizer constants
+const VU_COUNT = 16
+const FREQ_MAX = 11050.0
+
+const WIDTH = 300
+const HEIGHT = 250
+const HEIGHT_SCALE = 8.0
+const MIN_DB = 60
+
+const SAMPLE_COUNT = 100
+
 @export var ciphered_text: CipheredText
 @export var ciphered_image: CipheredImage
 @export var ciphered_audio: CipheredAudio
+
+var spectrum: AudioEffectSpectrumAnalyzerInstance
+var spectrum_points = PackedVector2Array()
+
+var _current_selection: CipherData.CipherType = CipherData.CipherType.TEXT:
+	set(value):
+		_current_selection = value
+		_display_cipher()
 
 @onready var _rendered_text = $Signal/RenderedText
 @onready var _rendered_image = $Signal/RenderedImage
@@ -10,12 +29,17 @@ class_name Monitor extends Control
 
 
 func _ready() -> void:
-	_display_cipher(CipherData.CipherType.TEXT)
+	_display_cipher()
 	ciphered_text.on_render.connect(_on_text_render)
 
+	# setup spectrum visualizer
+	spectrum = AudioServer.get_bus_effect_instance(AudioServer.get_bus_index("CipheredSignal"), 0)
+	spectrum_points.resize(SAMPLE_COUNT)
+	spectrum_points.fill(Vector2.ZERO)
 
-func _display_cipher(selection: CipherData.CipherType):
-	match selection:
+
+func _display_cipher():
+	match _current_selection:
 		CipherData.CipherType.TEXT:
 			_rendered_text.visible = true
 			_rendered_image.visible = false
@@ -28,6 +52,44 @@ func _display_cipher(selection: CipherData.CipherType):
 			_rendered_text.visible = false
 			_rendered_image.visible = false
 			_rendered_audio.visible = true
+
+
+func _draw():
+	# draw spectrum visualizer
+	draw_polyline(spectrum_points, Color.GREEN, 2.0, true)
+
+
+func _process(_delta: float) -> void:
+	match _current_selection:
+		CipherData.CipherType.AUDIO:
+			_update_audio_data()
+			queue_redraw()
+
+
+func _update_audio_data() -> void:
+	# inspiration from https://github.com/godotengine/godot-demo-projects/blob/master/audio/spectrum/show_spectrum.gd
+	if spectrum == null:
+		return
+
+	var data = []
+	var prev_hz = 0
+
+	for i in range(1, VU_COUNT + 1):
+		var hz = i * FREQ_MAX / VU_COUNT
+		var magnitude = spectrum.get_magnitude_for_frequency_range(prev_hz, hz).length()
+		var energy = clampf((MIN_DB + linear_to_db(magnitude)) / MIN_DB, 0, 1)
+		data.append(energy)
+		prev_hz = hz
+
+	for i in range(SAMPLE_COUNT):
+		var x = float(i) / SAMPLE_COUNT  # 0-1
+		var y = 0
+		for j in range(VU_COUNT):
+			var hz = (j + 1) * FREQ_MAX / VU_COUNT
+			y += sin(x * hz / FREQ_MAX * 100) * data[j]
+		y /= VU_COUNT
+		y *= HEIGHT * 5
+		spectrum_points.set(i, Vector2(x * WIDTH - WIDTH / 2.0, y))
 
 
 func _on_text_render() -> void:
@@ -44,4 +106,4 @@ func _on_audio_render() -> void:
 
 
 func _on_level_new_cipher_loaded(cipher_data: CipherData) -> void:
-	_display_cipher(cipher_data.cipher_type)
+	_current_selection = cipher_data.cipher_type
