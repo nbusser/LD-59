efd6fccdf0e45b87c41568f80d38308ee6624773
Nicolas Busser <nicolas.busser67@gmail.com>
2026-04-18 19:06:56 +0900

    add rot

--- src/CipheredSignal/signal_input.gd
+++ src/CipheredSignal/signal_input.gd
@@ -6,8 +6,11 @@ signal signal_input_changed(value: float)
 
 # 0.0 -> 1.0
 var amount: float = 0.5:
-    get(): return amount
-    set(new_amount):
-        amount = new_amount
-        emit_signal("signal_input_changed", amount)
+	get(): return amount
+	set(new_amount):
+		amount = new_amount
+		emit_signal("signal_input_changed", amount)
 
+
+func _on_v_slider_value_changed(value: float) -> void:
+	amount = value
