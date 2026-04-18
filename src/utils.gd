f681047dc88dfc77297445bb92b08e5cf9a85db4
Nicolas Busser <nicolas.busser67@gmail.com>
2026-04-18 21:53:45 +0900

    improve API

---
+++ src/utils.gd
@@ -0,0 +1,9 @@
+extends Node
+
+# Cannot use shuffle() when using a custom PRNG
+func shuffle_with_prng(array: Array, prng: RandomNumberGenerator):
+    for i in range(array.size() - 1, 0, -1):
+        var j = prng.randi_range(0, i)
+        var temp = array[i]
+        array[i] = array[j]
+        array[j] = temp
