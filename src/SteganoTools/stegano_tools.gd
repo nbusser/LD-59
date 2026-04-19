5cda1485ddd8ccd1875c16ba9756a8a3a8792883
Nicolas Busser <nicolas.busser67@gmail.com>
2026-04-19 22:28:47 +0900

    add missing assets

---
+++ src/SteganoTools/stegano_tools.gd
@@ -0,0 +1,51 @@
+class_name SteganoTools extends Control
+
+@export var _rendered_image: TextureRect
+
+@onready var _green_filter = $ImageFilters/FilterGreen
+@onready var _red_filter = $ImageFilters/FilterRed
+
+
+func _is_stegano_image() -> bool:
+	return (
+		Globals.current_level.level_state.phase == LevelState.Phase.STEGANO
+		and Globals.get_current_cipher().cipher_type == CipherData.CipherType.IMAGE
+	)
+
+
+func _on_level_phase_changed(phase: LevelState.Phase) -> void:
+	match phase:
+		LevelState.Phase.STEGANO:
+			visible = true
+			if _is_stegano_image():
+				_rendered_image.material.set_shader_parameter("enable_filter", true)
+				_rendered_image.material.set_shader_parameter(
+					"stegano_texture_sampler_green",
+					Globals.get_current_cipher().image_texture_filter_green
+				)
+				_rendered_image.material.set_shader_parameter(
+					"filter_green_size",
+					Globals.get_current_cipher().image_texture_filter_green.get_size()
+				)
+				_rendered_image.material.set_shader_parameter(
+					"stegano_texture_sampler_red",
+					Globals.get_current_cipher().image_texture_filter_red
+				)
+				_rendered_image.material.set_shader_parameter(
+					"filter_red_size",
+					Globals.get_current_cipher().image_texture_filter_red.get_size()
+				)
+		_:
+			visible = false
+
+
+func _process(_delta: float):
+	_green_filter.position = get_viewport().get_mouse_position() + Vector2(10, 10)
+
+	if _is_stegano_image():
+		_rendered_image.material.set_shader_parameter(
+			"filter_green_position", _green_filter.global_position
+		)
+		_rendered_image.material.set_shader_parameter(
+			"filter_red_position", _red_filter.global_position
+		)
