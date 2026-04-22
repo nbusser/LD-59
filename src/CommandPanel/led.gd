374dc05613268b11fdde93456e6fda5f828aeb93
Antoine Brûlé <antoine.brule@netc.eu>
2026-04-22 10:28:33 +0200

    add muted indicator

--- src/CommandPanel/led.gd
+++ src/CommandPanel/led.gd
@@ -1,3 +1,4 @@
+class_name Led
 extends Control
 
 @export var enabled = true:
--- src/Radio/radio.gd
+++ src/Radio/radio.gd
@@ -1,18 +1,24 @@
 class_name Radio
 extends Control
 
-var _muted := false
+var _muted := false:
+	set(value):
+		_muted = value
+		_update_mute()
+
 var _sfx_volume := 1.0
 var _music_volume := 1.0
 
 @onready var sfx_slider: HSlider = %SfxSlider
 @onready var music_slider: HSlider = %MusicSlider
 @onready var mute_button: TextureButton = %MuteButton
+@onready var red_led: Led = %RedLed
 
 
 func _ready() -> void:
 	sfx_slider.value = _sfx_volume
 	music_slider.value = _music_volume
+	_update_mute()
 
 
 func _on_sfx_slider_value_changed(value: float) -> void:
@@ -33,6 +39,13 @@ func _on_music_slider_value_changed(value: float) -> void:
 
 func _on_mute_button_toggled(toggled: bool) -> void:
 	_muted = toggled
+
+
+func _update_mute() -> void:
+	red_led.enabled = !_muted
+	music_slider.editable = !_muted
+	sfx_slider.editable = !_muted
+
 	var sfx_db = -80.0 if _muted else linear_to_db(_sfx_volume)
 	var music_db = -80.0 if _muted else linear_to_db(_music_volume)
 	AudioServer.set_bus_volume_db(AudioServer.get_bus_index("SFX"), sfx_db)
